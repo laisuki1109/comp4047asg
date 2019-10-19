e6f24761e924ba55fdbf559a3419648a71d54a54
laisuki1109 <laisuki1109@gmail.com>
2019-10-19 12:27:44 +0800

    first init

---
+++ src/com/company/Main.java
@@ -0,0 +1,169 @@
+package com.company;
+
+import javax.swing.text.MutableAttributeSet;
+import javax.swing.text.html.HTML;
+import javax.swing.text.html.HTMLEditorKit;
+import javax.swing.text.html.parser.ParserDelegator;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Enumeration;
+import java.util.List;
+
+class MyParserCallback extends HTMLEditorKit.ParserCallback {
+    public String content = new String();
+    public List<String> urls = new ArrayList<String>();
+    public Boolean isStyle=false;
+    // Override features of the parent's class
+    @Override
+    public void handleText(char[] data, int pos) {
+    	if(!isStyle) {
+    		content += new String(data) + " ";
+    		}
+        
+    }
+    
+    // Override handleStartTag() of the parent's class
+    @Override
+    public void handleStartTag(HTML.Tag tag, MutableAttributeSet attrSet, int pos)
+    {
+    	isStyle=false;
+    	// when calling this function is will save the url as create new column "urls" in callback
+    	//then the developer can call callback.url to get all url
+        if (tag.toString().equals("a")) {
+            Enumeration e = attrSet.getAttributeNames();
+            while (e.hasMoreElements()) {
+                Object aname = e.nextElement();
+                if (aname.toString().equals("href")) {
+                    String u = (String) attrSet.getAttribute(aname);
+                    if (urls.size() < 1024 && !urls.contains(u))
+                        urls.add(u);
+                }
+            }
+        }
+        else if (tag == HTML.Tag.STYLE) {
+        	System.out.println("Style tag");
+        	isStyle=true;
+        }
+//        }else if(tag== HTML.Tag.SCRIPT) {
+//        	System.out.println("Script tag");
+//        }
+    }
+    
+
+}
+
+public class Main {
+    //      get the html content without tags
+    // the function call Parse package function
+    public static String loadPlainText(String urlString) throws IOException {
+        MyParserCallback callback = new MyParserCallback();
+        ParserDelegator parser = new ParserDelegator();
+
+        URL url = new URL(urlString);
+        InputStreamReader reader = new InputStreamReader(url.openStream());
+        parser.parse(reader, callback, true); // call MyParserCallback to process the URL stream
+
+        return callback.content;
+    }
+    
+    //The function to call Parser.CallBackHandle start tags
+    public static List<String> getURLs(String srcPage) throws IOException {
+        URL url = new URL(srcPage);
+        InputStreamReader reader = new InputStreamReader(url.openStream());
+
+        ParserDelegator parser = new ParserDelegator();
+        MyParserCallback callback = new MyParserCallback();
+        parser.parse(reader, callback, true);
+        //parse the result to callback
+        for (int i=0; i<callback.urls.size(); i++) { 
+            String str = callback.urls.get(i);
+            if (!isAbsURL(str)) {
+                callback.urls.set(i, toAbsURL(str, url).toString());
+            }
+        }
+        return callback.urls;
+    }
+
+    // The function to separate string output
+    public static List<String> getUniqueWords(String text) {
+        String[] words = text.split("[\\d\\W]+");
+        ArrayList<String> uniqueWords = new ArrayList<String>();
+
+        for (String w : words) {
+            w = w.toLowerCase();
+
+            if (!uniqueWords.contains(w))
+                uniqueWords.add(w);
+        }
+        // abc order (joe instant function(?)
+        uniqueWords.sort(new Comparator<String>() {
+            @Override
+            public int compare(String a, String b) {
+                return a.compareTo(b);
+            }
+        });
+
+        return uniqueWords;
+    }
+    
+    //check it is an absoulte url
+    public static boolean isAbsURL(String str) {
+        return str.matches("^[a-z0-9]+://.+");
+    }
+
+    //convert to absoulte url
+    public static URL toAbsURL(String str, URL ref) throws MalformedURLException {
+        URL url = null;
+
+        String prefix =  ref.getProtocol() + "://" + ref.getHost()+ ref.getPath();
+
+        if (ref.getPort() > -1)
+            prefix += ":" + ref.getPort();
+
+        if (!str.startsWith("/")) {
+            int len = ref.getPath().length() - ref.getFile().length();
+            String tmp = ref.getPath().substring(0, len) + "/";
+            prefix += tmp.replace("//", "/");
+        }
+        url = new URL(prefix + str);
+
+        return url;
+    }
+
+
+
+    //get all the html content with tag
+ /*
+    static String loadWebPage(String urlString) {
+        byte[] buffer = new byte[1024];
+        String content = new String();
+        try {
+            URL url = new URL(urlString);
+            InputStream in = url.openStream();
+            int len;
+            while((len = in.read(buffer)) != -1)
+                content += new String(buffer);
+        } catch (IOException e) {
+            content = "<h1>Unable to download the page</h1>" + urlString;
+        }
+        return content;
+    }
+*/
+    public static void main(String[] args) throws IOException {
+        // write your code here
+        String url = "http://comp.hkbu.edu.hk/v1/";
+            System.out.println(loadPlainText(url));
+            System.out.println(getUniqueWords((loadPlainText(url))));
+            System.out.println(getURLs(url));
+
+        System.out.println("hello");
+
+
+    }
+
+}
